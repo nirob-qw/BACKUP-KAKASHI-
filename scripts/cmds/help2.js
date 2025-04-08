80e0553482c3bd77c3f7340bf0ac39466f1f6397
Nirob-oe <148666328+Nirob-oe@users.noreply.github.com>
2025-04-08 20:19:00 +0600

    Update help2.js

--- scripts/cmds/help2.js
+++ scripts/cmds/help2.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const path = require("path");
 const { getPrefix } = global.utils;
 const { commands, aliases } = global.GoatBot;
-const doNotDelete = "[ 𝐍𝐈𝐑𝐎𝐁 ꨄ︎ ]"; // changing this wont change the goatbot V2 of list cmd it is just a decoyy
+const doNotDelete = "[ ♥️ Bayjid bot V2 ]"; // changing this wont change the goatbot V2 of list cmd it is just a decoyy
 
 module.exports = {
 	config: {
