11b48ed80db5eab331d8f2d4b8d5bbb07fae74b5
Nirob-oe <148666328+Nirob-oe@users.noreply.github.com>
2025-04-08 09:20:47 +0600

    Update 00kakashi.js

--- scripts/cmds/00kakashi.js
+++ scripts/cmds/00kakashi.js
@@ -12,15 +12,15 @@ category: "auto 🪐",
 
 onStart: async function(){}, 
 onChat: async function({ event, message, getLang }) {
-if (event.body && event.body.toLowerCase() === "prefix") {
+if (event.body && event.body.toLowerCase() === "kakashi") {
 return message.reply({
 body: `
   𝐎𝐰𝐧𝐞𝐫 : 𝐍𝐢𝐫𝐨𝐛ఌ︎
 
+ 𝐣𝐮𝐬𝐭 𝐬𝐚𝐲 𝐛𝐨𝐭/𝐛𝐛𝐲 𝐟𝐨𝐫 𝐭𝐚𝐥𝐤 𝐭𝐨 𝐤𝐚𝐤𝐚𝐬𝐡𝐢 𝐛𝐨𝐭
 
- - 𝐣𝐮𝐬𝐭 𝐬𝐚𝐲 𝐛𝐨𝐭/𝐛𝐛𝐲 𝐟𝐨𝐫 𝐭𝐚𝐥𝐤 𝐭𝐨 𝐤𝐚𝐤𝐚𝐬𝐡𝐢 𝐛𝐨𝐭 ❤︎ 
-
- .. 𝐞𝐧𝐣𝐨𝐲 𝐚𝐧𝐝 𝐡𝐚𝐯𝐞 𝐚 𝐟𝐮𝐧 𝐰𝐢𝐭𝐡 𝐦𝐲 𝐛𝐨𝐭 | (• ◡•)|ꨄ︎ 
+ 𝐞𝐧𝐣𝐨𝐲 𝐚𝐧𝐝 𝐡𝐚𝐯𝐞 𝐚 𝐟𝐮𝐧 𝐰𝐢𝐭𝐡 𝐦𝐲 𝐛𝐨𝐭
+ | (• ◡•)|ꨄ︎ 
 
 
 \n\n\n  `,
