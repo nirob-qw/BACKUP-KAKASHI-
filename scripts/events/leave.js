de5f0b7789c00c9361af5ce0cfbce40cb57665eb
nirob-qw <148666597+nirob-qw@users.noreply.github.com>
2025-04-10 13:45:55 +0600

    Update leave.js

--- scripts/events/leave.js
+++ scripts/events/leave.js
@@ -2,32 +2,29 @@ const axios = require("axios");
 const fs = require("fs-extra");
 const request = require("request");
 module.exports = {
-	config: {
-		name: "Out",
-		aliases: ["l"],
-		version: "1.0",
-		author: "Sandy",
-		countDown: 5,
-		role: 2,
-		shortDescription: "bot will leave gc",
-		longDescription: "",
-		category: "admin",
-		guide: {
-			vi: "{pn} [tid,blank]",
-			en: "{pn} [tid,blank]"
-		}
-	},
+config: {
+name: "Out",
+aliases: ["l"],
+version: "1.0",
+author: "Sandy",
+countDown: 5,
+role: 2,
+shortDescription: "bot will leave gc",
+longDescription: "",
+category: "admin",
+guide: {
+vi: "{pn} [tid,blank]",
+en: "{pn} [tid,blank]"
+}
+},
 
-	onStart: async function ({ api,event,args, message }) {
- var id;
- if (!args.join(" ")) {
- id = event.threadID;
- } else {
- id = parseInt(args.join(" "));
- }
- return api.sendMessage('▣𝐊𝐚𝐤𝐚𝐬𝐡𝐢 𝐁𝐨𝐭  𝗟𝗘𝗔𝗩𝗘:\n》Group chhara mane relation sesh na just connection weak
-Jara mone rakhbe, tara rakhbe anyway
-
-Logging out Stay chill stay real\n\n➤𝑶𝒀 𝑴𝑨𝑴𝑴𝑰 𝑹𝑬', id, () => api.removeUserFromGroup(api.getCurrentUserID(), id))
-		}
-	}
+onStart: async function ({ api,event,args, message }) {
+var id;
+if (!args.join(" ")) {
+id = event.threadID;
+} else {
+id = parseInt(args.join(" "));
+}
+return api.sendMessage('▣KAKASHI BOT  𝗟𝗘𝗔𝗩𝗘:\n》Ami toder sukh dewar jonno Ashchilam tora etar joggo na.\n\n➤ oy mammi ree', id, () => api.removeUserFromGroup(api.getCurrentUserID(), id))
+}
+}
